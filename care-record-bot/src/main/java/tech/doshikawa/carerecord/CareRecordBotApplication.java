cacaafae16c23da59a1799b0952a8d9281901d0e
horinoA <horinoakiko@gmail.com>
2026-06-11 07:01:23 +0900

    springbootプロジェクト設定

---
+++ care-record-bot/src/main/java/tech/doshikawa/carerecord/CareRecordBotApplication.java
@@ -0,0 +1,13 @@
+package tech.doshikawa.carerecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareRecordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareRecordBotApplication.class, args);
+	}
+
+}
---
+++ care-record-bot/src/test/java/tech/doshikawa/carerecord/CareRecordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.doshikawa.carerecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareRecordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
